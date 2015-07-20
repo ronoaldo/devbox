67572f73029769b36a9305ff6155838c88291a99
Ronoaldo JLP <ronoaldo@gmail.com>
2015-07-20 20:18:20 +0000

    Serving IDE from port 80, with Docker support

--- startup.sh
+++ startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-set -e
+# Debug build/startup
+if [ x"$DEBUG" == x"true" ] ; then
+	set -e
+	set -x
+fi
 
 # Quiet setup
 export DEBIAN_FRONTEND=noninteractive
@@ -18,20 +22,37 @@ apt-get install --yes \
 	python-dev python3-dev \
 	golang golang-go.tools \
 	ruby ruby-dev \
-	mysql-client sqlite3
+	docker.io \
+	mysql-client sqlite3 \
+	nginx
 
 # Install newer maven
-MAVEN_BIN="http://ftp.unicamp.br/pub/apache/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz"
-mkdir -p /opt/maven
-curl $MAVEN_BIN | tar xzf - -C /opt/maven
-echo 'PATH=$PATH:/opt/maven/apache-maven-3.3.3/bin' >> /etc/profile
+which mvn || {
+	MAVEN_BIN="http://ftp.unicamp.br/pub/apache/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz"
+	mkdir -p /opt/maven
+	curl $MAVEN_BIN | tar xzf - -C /opt/maven
+	ln -s /opt/maven/apache-maven-3.3.3/bin/mvnDebug /usr/local/bin/mvnDebug
+	ln -s /opt/maven/apache-maven-3.3.3/bin/mvn /usr/local/bin/mvn
+}
 
-# Install IDE and User
-npm -g install codebox
+# Install IDE
+which codebox || {
+	npm -g install codebox
+	cd /usr/local/lib/node_modules/codebox/node_modules/shux/node_modules/pty.js/lib/pty.js
+	make build
+	make
+}
+
+# Setup the developer account
 adduser --gecos "" --disabled-password developer
+addgroup developer docker
 
 # Launch IDE on boot
 cp codeboxd /etc/init.d/codeboxd
 chmod +x /etc/init.d/codeboxd
 update-rc.d codeboxd defaults
 service codeboxd start
+
+# Expose on port 80
+cp codebox.conf /etc/nginx/conf.d/
+service nginx reload
